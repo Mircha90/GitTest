dabef699bf1cbdbf16809d0c87c644408bece75c
Mircha90 <mircha90@gmail.com>
2016-09-08 00:36:35 +0200

    Added project files

---
+++ Source/GitTest/GitTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GitTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITTEST_API AGitTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
